f4eaf96d3a95936fda7ccc33238c8b0b14c8be4b
Bre01cc <boliveiraassisreis@gmail.com>
2025-09-16 14:12:06 -0300

    Trocando de api

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 'use strict'
 //target(alvo)guarda onde o evento aconteceu
 
- async function pesquisarCep(cep){
+ async function pesquisarCep(){
     // https://corsproxy.io/?url
-    const url = `https://cdn.apicep.com/file/apicep/${cep}.jsons`
+    const url = `https://brasilapi.com.br/api/cep/v1/01025-020`
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data)
     return data
 }
-// const url = `https://cdn.apicep.com/file/apicep/01022-000.json`
-// const response = await fetch(url)
-// const data = await response.json()
-// console.log(data)
+
 
 async function preencherCampos({ target }) {
     const infoCep = await pesquisarCep(target.value)
-    document.getElementById('endereco').value= infoCep.address
-    document.getElementById('bairro').value=infoCep.district
+    document.getElementById('endereco').value= infoCep.neighborhood
+    document.getElementById('bairro').value=infoCep.street
     document.getElementById('estado').value=infoCep.state
     document.getElementById('cidade').value=infoCep.city
-    console.log(infoCep)
 }
 
 document.getElementById('cep').addEventListener('focusout', preencherCampos)
